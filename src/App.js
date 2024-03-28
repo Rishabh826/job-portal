4cc710a58634da4975a511e067b4defcafd98261
Rishabh826 <rishabhjangid0707@gmail.com>
2024-03-28 23:27:36 +0530

    thursday work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Login from './components/Login';
-import Navebar from './components/Navebar'; 
+import Navebar from './components/Navebar';
 import Signup from './components/Signup';
-import Contact from './components/Contact'; 
-import Home from './components/Home'; 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Employ from './components/Employ';
 
-export default function App() {
+const NavebarWithConditionalRender = () => {
+  const location = useLocation();
+  const shouldRenderNavebar = location.pathname !== '/Employ'; // Check if current route is not Employ
+
+  return shouldRenderNavebar ? <Navebar /> : null;
+};
+
+const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Navebar />
+        <NavebarWithConditionalRender /> {/* Render Navebar conditionally */}
         
         <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/Home" element={<Home />} />
-          <Route exact path="/Ioperson" element={<Login />} />
-          <Route exact path="/Signup" element={<Signup />} />
-          <Route exact path="/ContactUs" element={<Contact />} /> 
-          </Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Employ" element={<Employ />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/ContactUs" element={<Contact />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
+export default App;
